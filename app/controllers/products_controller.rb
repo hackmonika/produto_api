a67efec960a5b6fcd3e153dc8835d3edbdd3e556
monikahackmayer <mhackmayer@gmail.com>
2024-11-15 16:55:11 -0300

    criei controller de products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,47 @@
 class ProductsController < ApplicationController
+  # GET /products
+  def index
+    @products = Product.all
+    render json: @products
+  end
+
+  # GET /products/:id
+  def show
+    @product = Product.find(params[:id])
+    render json: @product
+  end
+
+  # POST /products
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      render json: @product, status: :created
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PUT /products/:id
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /products/:id
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    head :no_content
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :stock_quantity, :description)
+  end
 end
+
