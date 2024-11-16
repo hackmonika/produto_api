7dc4cdff95265a156fdee29d16147449515c7a75
monikahackmayer <mhackmayer@gmail.com>
2024-11-16 15:24:04 -0300

    adicionei rating a products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_15_193725) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_16_182023) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,5 +21,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_15_193725) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating"
   end
 end
