3ba64c8e62ccc7a31956cbb130ef13c0cdb4c745
monikahackmayer <mhackmayer@gmail.com>
2024-11-16 16:04:58 -0300

    criando controller de sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
-  def new
+  protect_from_forgery with: :null_session
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: { message: "User was created successfully!", user: @user }, status: :created
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
